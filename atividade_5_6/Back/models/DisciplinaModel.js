80d89d6cf2a60024236309ea7d432b5e462b5dde
Erica Sousa <ericams175@gmail.com>
2020-08-26 09:53:08 -0300

    Mudando nome da pasta.

---
+++ atividade_5_6/Back/models/DisciplinaModel.js
@@ -0,0 +1,9 @@
+class DisciplinaModel {
+    constructor(id, nome, curso, capacidade) {
+        this.id = id
+        this.nome = nome
+        this.curso = curso
+        this.capacidade = capacidade
+    }
+}
+module.exports = DisciplinaModel;
