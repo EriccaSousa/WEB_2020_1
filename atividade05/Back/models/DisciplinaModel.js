a0fe5135e2cf324aaa8755ce3a13a65f598ec970
Erica Sousa <ericams175@gmail.com>
2020-08-26 09:58:58 -0300

    Deletando pasta duplicada.

--- atividade05/Back/models/DisciplinaModel.js
+++
@@ -1,9 +0,0 @@
-class DisciplinaModel {
-    constructor(id, nome, curso, capacidade) {
-        this.id = id
-        this.nome = nome
-        this.curso = curso
-        this.capacidade = capacidade
-    }
-}
-module.exports = DisciplinaModel;
