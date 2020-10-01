cec132e8a44604ce39ae3eb254b75de26a548144
Érica Sousa <ericams175@gmail.com>
2020-10-01 10:37:39 -0300

    Add funções delete, update e retrieve.

--- atividade08/routes/DisciplinaRoute.js
+++ atividade08/routes/DisciplinaRoute.js
@@ -10,4 +10,17 @@ router.get('/list', function (req, res, next) {
   DisciplinaService.list(req, res);
 });
 
+router.put('/update/:id', function (req, res, next) {
+  DisciplinaService.update(req, res);
+});
+
+router.delete('/delete/:id', function (req, res, next) {
+  DisciplinaService.delete(req, res);
+});
+
+router.get('/retrieve/:id', function (req, res, next) {
+  DisciplinaService.retrieve(req, res);
+});
+
+
 module.exports = router;
--- atividade08/services/DisciplinaService.js
+++ atividade08/services/DisciplinaService.js
@@ -29,6 +29,48 @@ class DisciplinaService {
                 }
             )
     }
+    static update(req, res) {
+        DisciplinaModel.findByIdAndUpdate(req.params.id, req.body, { 'new': true })
+            .then(
+                (disciplina) => {
+                    res.status(201).json(disciplina);
+                }
+            )
+            .catch(
+                (error) => {
+                    res.status(500).json(error);
+                }
+            )
+    }
+
+    //retorna o user deletado
+    static delete(req, res) {
+        DisciplinaModel.findByIdAndRemove(req.params.id).then(
+            (disciplina) => {
+                res.status(201).json(disciplina);
+            }
+        )
+            .catch(
+                (error) => {
+                    res.status(500).json(error);
+                }
+            )
+    }
+    //retorna um user
+    static retrieve(req, res) {
+        DisciplinaModel.findById(req.params.id)
+            .then(
+                (disciplina) => {
+                    res.status(201).json(disciplina);
+                }
+            )
+            .catch(
+                (error) => {
+                    res.status(500).json(error);
+                }
+            )
+    }
 }
+
 
 module.exports = DisciplinaService;
